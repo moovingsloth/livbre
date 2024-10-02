0967fbac00465c2ef9d634bdc36b4f3f5179b97a
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-10-03 01:15:36 +0900

    로그인/로그아웃 기능 구현 완료

--- services/passport.js
+++ services/passport.js
@@ -5,13 +5,33 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {   
-        new User({ googleId: profile.id }).save();
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    })
+}); 
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id }).save().then(user => done(null, user));
+        }
+      });
     }
-    )
+  )
 );
